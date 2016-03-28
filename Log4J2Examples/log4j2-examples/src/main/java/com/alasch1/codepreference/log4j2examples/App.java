146cd2c46afbb7e8f5004c556b39a1c1aa88cfe0
Ala Schneider <aschneider@interwise.com>
2016-03-28 12:24:46 +0300

    log4j2examples project skeleton

---
+++ Log4J2Examples/log4j2-examples/src/main/java/com/alasch1/codepreference/log4j2examples/App.java
@@ -0,0 +1,13 @@
+package com.alasch1.codepreference.log4j2examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
