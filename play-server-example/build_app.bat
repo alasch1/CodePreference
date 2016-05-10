e91af25542942e9caa0e00da4b7fc83c34b9c4ba
Ala Schneider <aschneider@interwise.com>
2016-05-10 10:07:43 +0300

    Play example was converted to maven project

---
+++ play-server-example/build_app.bat
@@ -0,0 +1,35 @@
+@ECHO ON
+
+REM		Hard coded information
+SET JAVA_HOME=%JDK_8%
+SET SBT_COMMAND_TEST=test
+SET SBT_COMMAND=dist
+
+
+REM		Execute the SBT tests. (temporary commented out)
+::	echo Execute the SBT tests.
+::	CALL sbt.bat -Dsbt.repository.config=repositories_windows.txt %SBT_COMMAND_TEST%
+::	if %ERRORLEVEL% NEQ 0 GOTO BAD_EXECUTION
+::	echo OK.
+::	echo.
+
+
+REM		Execute the SBT.
+echo Execute the SBT.
+CALL sbt.bat -Dsbt.repository.config=repositories_windows.txt %SBT_COMMAND%
+if %ERRORLEVEL% NEQ 0 GOTO BAD_EXECUTION
+echo OK.
+echo.
+
+
+
+REM		Exit OK.
+exit /b 0
+
+
+:BAD_EXECUTION
+echo.
+echo Error while executing the activator.
+echo.
+exit /b 2
+
