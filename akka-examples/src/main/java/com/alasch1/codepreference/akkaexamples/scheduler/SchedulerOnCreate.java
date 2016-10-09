7ee4243709af9294fa494b123a8cab88904cb086
Ala Schneider <aschneider@interwise.com>
2016-10-09 14:55:57 +0300

    SchedulerOnCreate better implementation

--- akka-examples/src/main/java/com/alasch1/codepreference/akkaexamples/scheduler/SchedulerOnCreate.java
+++ akka-examples/src/main/java/com/alasch1/codepreference/akkaexamples/scheduler/SchedulerOnCreate.java
@@ -16,21 +16,43 @@ public class SchedulerOnCreate extends AbstractLoggingActor {
 	
 	// Protocol
 	private static final String TICK = "tick";
+	private static final String START = "start";
 	
 	private static final String ACTOR_SYSTEM = "MySystem";
 	private static final String ACTOR = "scheduler";
 	
-	public static Props props() {
-		return Props.create(SchedulerOnCreate.class, () -> new SchedulerOnCreate());
+	private Cancellable tick;
+	private long intervalSecs;
+	
+	public static Props props(long intervalSecs) {
+		return Props.create(SchedulerOnCreate.class, () -> new SchedulerOnCreate(intervalSecs));
 	}
 	
-	public SchedulerOnCreate() {
+	public SchedulerOnCreate(long intervalSecs) {
+		this.intervalSecs = intervalSecs;
 		receive(ReceiveBuilder
 			.matchEquals(TICK, this::onTick)
+			.matchEquals(START, m -> schedule())
 			.matchAny(m -> unhandled(m))
 			.build());
 	}
 	
+	private void schedule() {
+    	tick = getContext().system().scheduler().schedule(
+    			Duration.Zero(), 
+    			Duration.create(intervalSecs, TimeUnit.SECONDS),
+    			self(), 
+    			TICK,
+    			getContext().dispatcher(), 
+    			null);
+	}	
+	
+	@Override
+	public void postStop() throws Exception {
+		tick.cancel();
+		super.postStop();
+	}
+
 	private void onTick(String tick) {
 		log().info("ticking ...");
 	}
@@ -38,19 +60,9 @@ public class SchedulerOnCreate extends AbstractLoggingActor {
     public static void main( String[] args ) {
     	
     	ActorSystem system = ActorSystem.create(ACTOR_SYSTEM);
-    	ActorRef scheduler = system.actorOf(SchedulerOnCreate.props(), ACTOR);
-    	
-    	// If a tick is a data member, interval is ignored
-    	Cancellable tick = system.scheduler().schedule(
-    			Duration.create(10, TimeUnit.SECONDS), 
-    			Duration.create(10, TimeUnit.SECONDS),
-    			scheduler, 
-    			TICK,
-    			system.dispatcher(), 
-    			null);
-
-        AppUtil.terminate(system, scheduler);
-        tick.cancel();
+    	ActorRef actor = system.actorOf(SchedulerOnCreate.props(10), ACTOR);
+    	actor.tell(START, ActorRef.noSender());
+        AppUtil.terminate(system, actor);
         
     }
 }
