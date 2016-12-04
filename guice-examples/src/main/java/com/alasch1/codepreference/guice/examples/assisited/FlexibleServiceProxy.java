8ed9b481250ea1ff10e4b714d9eb55ae6e9d8b22
Ala Schneider <aschneider@interwise.com>
2016-12-04 07:33:47 +0200

    Guice examples improvements

--- guice-examples/src/main/java/com/alasch1/codepreference/guice/examples/assisited/FlexibleServiceProxy.java
+++ guice-examples/src/main/java/com/alasch1/codepreference/guice/examples/assisited/FlexibleServiceProxy.java
@@ -2,24 +2,32 @@ package com.alasch1.codepreference.guice.examples.assisited;
 
 import javax.inject.Inject;
 
-import com.google.inject.Injector;
+import com.alasch1.codepreference.guice.examples.DIContainer;
+import com.alasch1.codepreference.guice.examples.basic.ServiceProvider;
 import com.google.inject.Key;
-import com.google.inject.name.Names;
 import com.google.inject.assistedinject.Assisted;
+import com.google.inject.name.Names;
 
 public class FlexibleServiceProxy implements ServiceProxy {
 
-	private NamedServiceProvider service;
+	private NamedServiceProvider otherService;
+	private ServiceProvider injectedService;
 	
 	@Inject 
-	public FlexibleServiceProxy(Injector injector, @Assisted String serviceName) {
-		System.out.println("serviceName:" + serviceName);
-		service = injector.getInstance(Key.get(NamedServiceProvider.class, Names.named(serviceName)));
-		System.out.println(service.getName());
+	public FlexibleServiceProxy(ServiceProvider injectedService, @Assisted String otherServiceName) {
+		System.out.println("serviceName:" + otherServiceName);
+		this.injectedService = injectedService;
+		otherService = DIContainer.getInjector().getInstance(Key.get(NamedServiceProvider.class, Names.named(otherServiceName)));
+		System.out.println(otherService.getName());
 	}
 	
 	@Override
-	public NamedServiceProvider getService() {
-		return service;
+	public NamedServiceProvider getNamedService() {
+		return otherService;
 	}
+
+	public ServiceProvider getInjectedService() {
+		return injectedService;
+	}
+
 }
