507c5e1f280fe9c738aed72c377d3fd39883bcf1
aschneider <aschneider@interwise.com>
2016-12-10 21:53:57 +0200

    HackerRank solutions

---
+++ notepad/src/main/java/com/alasch1/notepad/hackerrank/ViralAdvertisingSolution.java
@@ -0,0 +1,28 @@
+package com.alasch1.notepad.hackerrank;
+
+import java.util.Scanner;
+
+public class ViralAdvertisingSolution {
+	
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int n = sc.nextInt();
+			if (n<1 || n>50) {
+				return;
+			}
+			
+			int likes = 0, recieved = 5, newLikes=0;
+			for (int day=1; day<=n; day++) {
+				// new likes
+				newLikes = Math.floorDiv(recieved,2);
+				likes += newLikes;
+				recieved = newLikes *3;
+			}
+			
+			System.out.println(likes);			
+		}
+
+	}
+
+}
+
