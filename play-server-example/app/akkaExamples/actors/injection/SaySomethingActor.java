eace4f6199805caa88ef2688986644ff1877c46b
aschneider <aschneider@interwise.com>
2016-12-23 18:43:40 +0200

    Example of Akka actor injection in play improved

--- play-server-example/app/akkaExamples/actors/injection/SaySomethingActor.java
+++ play-server-example/app/akkaExamples/actors/injection/SaySomethingActor.java
@@ -5,6 +5,7 @@ import com.google.inject.assistedinject.Assisted;
 
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import play.Configuration;
 
 public class SaySomethingActor extends UntypedActor {
 	
@@ -19,19 +20,23 @@ public class SaySomethingActor extends UntypedActor {
 		}
 	}
 
-	private String something;
-	
-	public static Props props(String something) {
-		return Props.create(SaySomethingActor.class, something);
+	// Factory method for explicit creation
+	public static Props props(String something, Configuration configuration) {
+		return Props.create(SaySomethingActor.class, something, configuration);
 	}
 	
+	private static final String SIGNATURE_PROPERTY = "signature";
+	private String something;	
+	private final Configuration configuration;
+	
 	@Inject
-	public SaySomethingActor(@Assisted String something) {
+	public SaySomethingActor(@Assisted String something, Configuration configuration) {
 		this.something = something;
+		this.configuration = configuration;
 	}
 	
 	private void sayHello(Someone someone) {
-		String helloString = something + " " +someone.getName();
+		String helloString = something + " " + someone.getName() + ", signature:" + configuration.getString(SIGNATURE_PROPERTY);
 		System.out.println(helloString);
 		sender().tell(helloString, self());
 	}
