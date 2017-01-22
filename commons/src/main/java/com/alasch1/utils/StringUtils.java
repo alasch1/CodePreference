ed72b38f24780eb4ec559c07b1b342476c7acf48
aschneider <aschneider@interwise.com>
2017-01-22 15:25:59 +0200

    Added StringUtils to commons

---
+++ commons/src/main/java/com/alasch1/utils/StringUtils.java
@@ -0,0 +1,71 @@
+package com.alasch1.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Implements general purpose utilities
+ * 
+ * @author aschneider
+ *
+ */
+
+/**
+ * Extracts email domain as the tail part of the mail after the first appearance of @ character
+ * If mail does not contain @ character the full email is retruened
+ * 
+ * @author aschneider
+ *
+ */
+public final class StringUtils {
+	
+	private static final String SPACE = " ";
+	private static final String ESC_SPACE="%20";
+
+	public static String getEmailDomain(String email) {
+		String[] mailParts = email.split("@");
+		if (mailParts.length > 1) {
+			return mailParts[1];
+		}
+		else {
+			return email;
+		}
+	}
+
+	public static String encodeUrl(String origUrl) {		
+		try {
+			return URLEncoder.encode(origUrl, "UTF-8");
+		} 
+		catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Make uncheked exception", e);
+		}
+	}
+
+	public static String escapeSpace(String origValue) {
+		return origValue.replace(SPACE, ESC_SPACE);
+	}
+
+	/**
+	 * Returns a string representation of a local current time in ISO format;
+	 * If a flag showNanos is true, presents a time with nano seconds, else truncates to seconds.
+	 * 
+	 * @param units
+	 * @return
+	 */
+	public static String localTimeISO(boolean showNanos) {
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+		if (showNanos) {
+			return formatter.format(date);
+		}
+		else {
+			return formatter.format(date.truncatedTo(ChronoUnit.SECONDS));
+		}
+	}
+
+	private StringUtils() {		
+	}
+}
---
+++ commons/src/test/java/com/alasch1/utils/TestStringUtils.java
@@ -0,0 +1,64 @@
+package com.alasch1.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.junit.Test;
+
+import com.alasch1.testutils.VerbalTestExecutor;
+
+public class TestStringUtils {
+
+	@Test
+	public void testGetValidDomain() {
+		VerbalTestExecutor.executeTest("testGetValidDomain", () -> {
+			String email = "ala@com.com";
+			String domain = StringUtils.getEmailDomain(email);
+			System.out.println("Domain:" + domain);
+			assertNotSame(email, domain);
+		});		
+	}
+	
+	@Test
+	public void testGetMissingDomain() {
+		VerbalTestExecutor.executeTest("testGetMissingDomain", () -> {
+			String email = "ala.com.com";
+			String domain = StringUtils.getEmailDomain(email);
+			System.out.println("Domain:" + domain);
+			assertEquals(email, domain);
+		});		
+	}
+	
+	@Test
+	public void testUrlEncode() throws UnsupportedEncodingException {
+		VerbalTestExecutor.executeTest("testUrlEncode", () -> {
+			String [] urls4test = {"a b", "a^b", "("};
+			for (int i=0; i<urls4test.length; i++) {
+				String origUrl = urls4test[i];
+				System.out.println("origUrl:" + origUrl);
+				String encoded = StringUtils.encodeUrl(origUrl);
+				System.out.println("encoded URL:" + encoded);
+				String restored = URLDecoder.decode(encoded, "UTF-8");
+				System.out.println("restored URL:" + restored);
+				assertEquals(origUrl, restored);
+			}
+		});		
+	}
+
+	
+	@Test
+	public void testLocalTimeISO() {
+		VerbalTestExecutor.executeTest("testLocalTimeISO", () -> {
+			String localInNanos = StringUtils.localTimeISO(true);
+			String localInSecs = StringUtils.localTimeISO(false);
+			System.out.println("localInNanos:" + localInNanos);
+			System.out.println("localInSecs:" + localInSecs);
+			assertTrue(localInNanos.length() > localInSecs.length());
+		});		
+	}
+
+}
